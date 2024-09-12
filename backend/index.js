80d563d7c631ec9b03c3819bad6149019c6fdd56
Amira Boubaker <amiraboubakeresprims@gmail.com>
2024-09-12 20:36:28 +0100

    mongodb connection using mongoose module

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const app = require('./app');
-
+const db = require('./config/db');
 const port = 3000; 
 
 app.get('/',(req,res)=>{
-    res.send("Hello World Amira 123!!!");
-})
+    res.send("Hello World!!!");
+});
+
 app.listen(port, ()=>{
     console.log(`Server Listening on Port http://localhost:${port}`);
 });
